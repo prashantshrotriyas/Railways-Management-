f0d588eb643620a9b72fca1654aa028fd1a1e98b
prashantshrotriyas <prashantshrotriyas@gmail.com>
2024-11-15 15:36:31 +0530

    Implemenation of Station class in Railways.cpp

--- RAILWAYS.cpp
+++ RAILWAYS.cpp
@@ -49,3 +49,37 @@ map<int, pair<bool, string>> &platform::get_schedule()
     return schedule;
 }
 
+// station class
+
+// Applying template for station class functions
+
+template <class T>
+T station<T>::get_st_id()
+{
+    return st_id;
+}
+
+template <class T>
+void station<T>::set_st_id(T id)
+{
+    st_id = id;
+}
+
+template <class T>
+string station<T>::get_st_name()
+{
+    return st_name;
+}
+
+template <class T>
+void station<T>::set_st_name(string name)
+{
+    st_name = name;
+}
+
+template <class T>
+void station<T>::printpltfrm()
+{
+    for (auto pt : pltfrm)
+        cout << pt.get_pt_id() << endl;
+}
