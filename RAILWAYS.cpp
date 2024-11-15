013ee94b64ca569493023aa158c72e9c15110699
prashantshrotriyas <prashantshrotriyas@gmail.com>
2024-11-15 15:31:01 +0530

    Implemenation of line class in RAILWAYS.cpp

---
+++ RAILWAYS.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "RAILWAYS.h"
+using namespace std;
+
+// function implementation of classes
+
+// line class
+void line::set_line_id(string id)
+{   
+    line_id = id;
+
+    
+}
+
+string line::get_line_id()
+{   
+    
+    return line_id;
+}
