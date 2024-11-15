11ad40cee0928206e58137dca604c5655b4db9a2
prashantshrotriyas <prashantshrotriyas@gmail.com>
2024-11-15 15:34:13 +0530

    Implemenation of platform class in RAILWAYS.cpp

--- RAILWAYS.cpp
+++ RAILWAYS.cpp
@@ -17,3 +17,35 @@ string line::get_line_id()
     
     return line_id;
 }
+
+// platform class
+
+void platform::set_pt_id(string id)
+{
+    pt_id = id;
+}
+
+string platform::get_pt_id()
+{
+    return pt_id;
+}
+
+void platform::print_pt_lines()
+{
+        cout <<  pt_lines.get_line_id() << endl;
+}
+line platform::get_pt_lines()
+{
+    return pt_lines;
+}
+void platform::set_pt_lines(string id)
+{
+    
+    pt_lines.set_line_id(id);
+}
+
+map<int, pair<bool, string>> &platform::get_schedule()
+{
+    return schedule;
+}
+
